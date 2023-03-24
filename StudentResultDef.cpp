4b95a8b72c404deec40f25bffc3fd37b3d736244
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-03-24 23:37:50 +0530

    [ADDED]:[Janhavi] Result class inherited Student class

---
+++ StudentResultDef.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+	public:
+		char name[20];
+		int id;
+	
+		void setStudent() {
+			cout<<"\nEnter the Name and ID :";
+			cin>>name>>id;
+		}
+		
+};
+
+class Result:public Student
+{
+	private:
+		float per;
+	public:
+		void setPer() {
+			cout<<"\nEnter the Percentage :";
+			cin>>per;
+		}	
+		void showResult() {
+			cout<<"\nName :"<<name;
+			cout<<"\nID :"<<id;
+			cout<<"\nPercentage :"<<per;
+		}
+};
+
+int main()
+{
+	Result R;
+	R.setStudent();
+	R.setPer();
+	R.showResult();
+	return 0;
+}
